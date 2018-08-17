586a9c88f9e8101a5e03d76961fdc01ef0bd4c41
Hanqi Guo <hguo@anl.gov>
2018-08-17 14:32:04 -0500

    WIP

--- include/ftk/algorithms/cca.h
+++ include/ftk/algorithms/cca.h
@@ -1,6 +1,33 @@
 #ifndef _CCA_H
 #define _CCA_H
 
+#include "ftk/transition/transitionMatrix.h"
+
+namespace ftk {
+
+template <class IdType>
+TransitionMatrix associateConnectedComponents(
+    const std::vector<std::set<IdType> > &components0,
+    const std::vector<std::set<IdType> > &components1) 
+{
+  auto checkIntersection = [](const std::set<IdType>& s0, const std::set<IdType>& s1) {
+    for (const auto &e0 : s0) {
+      if (s1.find(e0) != s1.end()) return true;
+    }
+    return false;
+  };
+
+  TransitionMatrix mat;
+
+  for (size_t i = 0; i < components0.size(); i ++) {
+    for (size_t j = 0; j < components1.size(); j ++) {
+      if (checkIntersection(components0[i], components1[j])) {
+        mat.set(i, j);
+      }
+    }
+  }
+}
+
 template <class IdType>
 std::vector<std::set<IdType> > extractConnectedComponents(
     const std::function<std::set<IdType>(size_t) >& neighbors,
@@ -54,6 +81,8 @@ std::vector<std::set<IdType> > extractConnectedComponents(
   fprintf(stderr, "#qualified=%zu\n", qualified.size());
 
   return extractConnectedComponents(neighbors, qualified);
+}
+
 }
 
 #endif
