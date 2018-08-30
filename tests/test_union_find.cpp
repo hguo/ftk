fafbaf1ce276fa04358d3dd096647a2534f8b74d
Hanqi Guo <hguo@anl.gov>
2018-08-30 12:30:57 -0500

    WIP: sweep and merge impl

--- tests/test_union_find.cpp
+++ tests/test_union_find.cpp
@@ -10,116 +10,99 @@
 
 template <class IdType>
 struct Tree {
-  void addNode(IdType i) { nodes.insert(i); }
+  void add_node(IdType i) { nodes.insert(i); }
+  bool has_node(IdType i) const { return nodes.find(i) != nodes.end(); }
 
-  bool hasNode(IdType i) const { return nodes.find(i) != nodes.end(); }
-
-  void addArc(IdType lo, IdType hi) {
-    upperLinks[lo].insert(hi);
-    lowerLinks[hi].insert(lo);
+  void add_arc(IdType lo, IdType hi) {
+    upper_links[lo].insert(hi);
+    lower_links[hi].insert(lo);
   }
- 
-  bool isLeaf(IdType i) const {
-    return upperDegree(i) + lowerDegree(i) == 1;
-  }
-
-  size_t upperDegree(IdType lo) const {
-    auto it = upperLinks.find(lo);
-    if (it != upperLinks.end()) return it->second.size();
-    else return 0;
-  }
-
-  size_t lowerDegree(IdType hi) const {
-    auto it = lowerLinks.find(hi);
-    if (it != lowerLinks.end()) return it->second.size();
-    else return 0;
-  }
-
-  IdType upperNode(IdType lo) const { // return the first upper node
-    auto it = upperLinks.find(lo);
-    if (it != upperLinks.end()) return *it->second.begin();
-    else return IdType(-1);
-  }
-
-  IdType lowerNode(IdType hi) const { // return the first lower node
-    auto it = lowerLinks.find(hi);
-    if (it != lowerLinks.end()) return *it->second.begin();
-    else return IdType(-1);
-  }
-
-  bool reduceNode(IdType i) { // remove a node whose up-degree and down-degree are both less or equal to 1
-    size_t ud = upperDegree(i), ld = lowerDegree(i);
-    // fprintf(stderr, "REDUCING NODE: ud=%zu, ld=%zu\n", ud, ld);
-    if (ud > 1 || ld > 1) return false; // cannot do the reduction
+  
+  bool reduce_node(IdType i) { // remove a node whose up-degree and down-degree are both less or equal to 1
+    size_t ud = upper_degree(i), ld = lower_degree(i);
+    if (ud > 1 || ld > 1) return false; // cannot do the node reduction
     else if (ud == 1 && ld == 0) {
-      lowerLinks[upperNode(i)].erase(i);
+      lower_links[upper_node(i)].erase(i);
     } else if (ud == 0 && ld == 1) {
-      upperLinks[lowerNode(i)].erase(i);
+      upper_links[lower_node(i)].erase(i);
     } else { // ud == 1 && ld == 1
-      auto hi = upperNode(i), lo = lowerNode(i);
-      lowerLinks[hi].erase(i);
-      upperLinks[lo].erase(i);
-      lowerLinks[hi].insert(lo);
-      upperLinks[lo].insert(hi);
+      auto hi = upper_node(i), lo = lower_node(i);
+      lower_links[hi].erase(i);
+      upper_links[lo].erase(i);
+      lower_links[hi].insert(lo);
+      upper_links[lo].insert(hi);
     }
     nodes.erase(i);
-    upperLinks.erase(i);
-    lowerLinks.erase(i);
+    upper_links.erase(i);
+    lower_links.erase(i);
     return true;
   }
+ 
+  bool is_leaf(IdType i) const {
+    return upper_degree(i) + lower_degree(i) == 1;
+  }
 
-  std::set<IdType> getUpperLinks(IdType i) const {
-    auto it = upperLinks.find(i);
-    if (it != upperLinks.end()) return it.second;
+  size_t upper_degree(IdType lo) const {
+    auto it = upper_links.find(lo);
+    if (it != upper_links.end()) return it->second.size();
+    else return 0;
+  }
+
+  size_t lower_degree(IdType hi) const {
+    auto it = lower_links.find(hi);
+    if (it != lower_links.end()) return it->second.size();
+    else return 0;
+  }
+
+  IdType upper_node(IdType lo) const { // return the first upper node
+    auto it = upper_links.find(lo);
+    if (it != upper_links.end()) return *it->second.begin();
+    else return IdType(-1);
+  }
+
+  IdType lower_node(IdType hi) const { // return the first lower node
+    auto it = lower_links.find(hi);
+    if (it != lower_links.end()) return *it->second.begin();
+    else return IdType(-1);
+  }
+
+  const std::set<IdType>& upper_nodes(IdType i) const {
+    auto it = upper_links.find(i);
+    if (it != upper_links.end()) return it.second;
     else return std::set<IdType>();
   }
 
-  std::set<IdType> getLowerLinks(IdType i) const {
-    auto it = lowerLinks.find(i);
-    if (it != lowerLinks.end()) return it.second;
+  const std::set<IdType>& lower_nodes(IdType i) const {
+    auto it = lower_links.find(i);
+    if (it != lower_links.end()) return it.second;
     else return std::set<IdType>();
   }
 
-  void printNeighbor(IdType n, const std::function<double(size_t)> &value) const {
-    std::stringstream ss;
-    ss << value(n) << ": ";
-    auto itu = upperLinks.find(n);
-    if (itu != upperLinks.end()) {
-      for (auto j : itu->second) {
-        ss << value(j) << ", ";
+  template <class ValueType>
+  void print_with_values(const std::function<ValueType(IdType)>& value) const {
+    for (const auto &kv : upper_links) {
+      IdType i = kv.first;
+      for (const auto j : kv.second) {
+        fprintf(stderr, "%zu(%f) <--> %zu(%f)\n", 
+            i, value(i), j, value(j));
       }
-    }
-    ss << ";";
-    auto itl = lowerLinks.find(n);
-    if (itl != lowerLinks.end()) {
-      for (auto j : itl->second) {
-        ss << value(j) << ", ";
-      }
-    }
-    fprintf(stderr, "%s | ud=%zu, ld=%zu\n", ss.str().c_str(), upperDegree(n), lowerDegree(n));
-  }
-
-  void print(const std::function<double(size_t)> &value) const {
-    for (auto n : nodes) {
-      printNeighbor(n, value);
     }
   }
 
 protected:
   std::set<IdType> nodes;
-  std::map<IdType, std::set<IdType> > upperLinks, lowerLinks;
+  std::map<IdType, std::set<IdType> > upper_links, lower_links;
 };
 
 template <class IdType>
-Tree<IdType> mergeTree(IdType nn, Tree<IdType>& jt, Tree<IdType>& st, const std::function<double(size_t)> &value) 
+Tree<IdType> merge_trees(IdType nn, Tree<IdType>& jt, Tree<IdType>& st)
 {
   Tree<IdType> ct;
   std::queue<IdType> Q;
 
   for (IdType i = 0; i < nn; i ++) {
-    ct.addNode(i);
-    if (jt.upperDegree(i) + st.lowerDegree(i) == 1) {
-      // fprintf(stderr, "enqueuing %f, ud=%zu, ld=%zu\n", value(i), jt.upperDegree(i), st.lowerDegree(i));
+    ct.add_node(i);
+    if (jt.upper_degree(i) + st.lower_degree(i) == 1) {
       Q.push(i);
     }
   }
@@ -128,30 +111,21 @@ Tree<IdType> mergeTree(IdType nn, Tree<IdType>& jt, Tree<IdType>& st, const std:
     auto i = Q.front();
     Q.pop();
 
-    // fprintf(stderr, "working on %f, ud=%zu, ld=%zu\n", value(i), jt.upperDegree(i), st.lowerDegree(i));
     size_t j;
-    if (jt.upperDegree(i) == 0 && st.lowerDegree(i) == 1) { // upper leaf
-      j = jt.lowerNode(i);
-      ct.addArc(j, i);
-      fprintf(stderr, "CTARC: %f <--> %f\n", value(j), value(i));
-    } else if (st.lowerDegree(i) == 0 && jt.upperDegree(i) == 1) { // lower leaf
-      j = st.upperNode(i);
-      ct.addArc(i, j);
-      fprintf(stderr, "CTARC: %f <--> %f\n", value(i), value(j));
+    if (jt.upper_degree(i) == 0 && st.lower_degree(i) == 1) { // upper leaf in split tree
+      j = jt.lower_node(i);
+      ct.add_arc(j, i);
+    } else if (st.lower_degree(i) == 0 && jt.upper_degree(i) == 1) { // lower leaf in join tree
+      j = st.upper_node(i);
+      ct.add_arc(i, j);
     } else {
-      // fprintf(stderr, "ERROR: %f, ud=%zu, ld=%zu\n", value(i), jt.upperDegree(i), st.lowerDegree(i));
-      continue; // no longer a leaf?
-      // assert(false);
+      continue; // i is no longer a leaf.
     }
       
-    jt.reduceNode(i);
-    st.reduceNode(i);
+    jt.reduce_node(i);
+    st.reduce_node(i);
 
-    // fprintf(stderr, "incident j(%f): ud=%zu, ld=%zu\n", value(j), jt.upperDegree(j), st.lowerDegree(j));
-    // jt.printNeighbor(j, value);
-    // st.printNeighbor(j, value);
-    if (jt.upperDegree(j) + st.lowerDegree(j) == 1) {
-      // fprintf(stderr, "enqueuing %f\n", value(j));
+    if (jt.upper_degree(j) + st.lower_degree(j) == 1) { // new leaf node
       Q.push(j);
     }
   }
@@ -159,57 +133,68 @@ Tree<IdType> mergeTree(IdType nn, Tree<IdType>& jt, Tree<IdType>& st, const std:
   return ct;
 }
 
-void buildTree(int nn,
-    const std::vector<size_t> &order,
-    const std::vector<size_t> &inverseOrder,
-    const std::function<double(size_t)> &value,
+template <class IdType>
+Tree<IdType> build_join_tree(IdType nn, 
+    const std::vector<IdType> &order,
+    const std::vector<IdType> &inverseOrder,
     const std::function<std::set<size_t>(size_t)> &neighbors)
 {
-  ftk::quick_union<size_t> uf(nn);
-  Tree<size_t> st, jt, ct;
+  ftk::quick_union<IdType> uf(nn);
+  Tree<IdType> jt;
 
-  // split tree
-  for (int i=0; i<nn; i++) {
-    st.addNode(order[i]);
-    // fprintf(stderr, "adding (%f)\n", value(order[i]));
-
-    for (auto j : neighbors(order[i])) {
-      j = inverseOrder[j];
-      if (j < i) {
-        size_t ri = uf.root(i), rj = uf.root(j);
-        if (ri != rj) {
-          st.addArc(order[rj], order[ri]);
-          // fprintf(stderr, "adding arc (root=%f) <--> (root=%f)\n", 
-          //     value(order[ri]), value(order[rj]));
-          uf.unite(rj, ri);
-        }
-      }
-    }
-  }
-
-  uf.reset();
-  // join tree
   for (int i=nn-1; i>=0; i--) {
-    jt.addNode(order[i]);
+    jt.add_node(order[i]);
     for (auto j : neighbors(order[i])) {
       j = inverseOrder[j];
       if (j > i) {
         size_t ri = uf.root(i), rj = uf.root(j);
         if (ri != rj) {
-          jt.addArc(order[ri], order[rj]);
+          jt.add_arc(order[ri], order[rj]);
           uf.unite(rj, ri);
         }
       }
     }
   }
 
-  // fprintf(stderr, "join tree:\n");
-  // jt.print(value);
-  // fprintf(stderr, "split tree:\n");
-  // st.print(value);
+  return jt;
+}
 
-  // merge tree
-  mergeTree<size_t>(nn, jt, st, value);
+template <class IdType>
+Tree<IdType> build_split_tree(IdType nn, 
+    const std::vector<IdType> &order,
+    const std::vector<IdType> &inverseOrder,
+    const std::function<std::set<size_t>(size_t)> &neighbors)
+{
+  ftk::quick_union<IdType> uf(nn);
+  Tree<IdType> st;
+
+  for (int i=0; i<nn; i++) {
+    st.add_node(order[i]);
+    for (auto j : neighbors(order[i])) {
+      j = inverseOrder[j];
+      if (j < i) {
+        size_t ri = uf.root(i), rj = uf.root(j);
+        if (ri != rj) {
+          st.add_arc(order[rj], order[ri]);
+          uf.unite(rj, ri);
+        }
+      }
+    }
+  }
+
+  return st;
+}
+
+template <class IdType>
+Tree<IdType> sweep_and_merge(IdType nn,
+    const std::vector<IdType> &order,
+    const std::vector<IdType> &inverseOrder,
+    const std::function<std::set<size_t>(size_t)> &neighbors)
+{
+  Tree<IdType> st = build_split_tree(nn, order, inverseOrder, neighbors), 
+               jt = build_join_tree(nn, order, inverseOrder, neighbors);
+  
+  return merge_trees<IdType>(nn, jt, st);
 }
 
 int main(int argc, char **argv)
@@ -241,9 +226,10 @@ int main(int argc, char **argv)
   for (size_t i=0; i<W*H; i++)
     inverseOrder[order[i]] = i;
 
-  buildTree(W*H, order, inverseOrder,
-      [&values](size_t i) {return values[i];}, 
-      std::bind(ftk::Get4Neighbors2DRegular<size_t>, W, H, std::placeholders::_1));
-  
+  auto ct = sweep_and_merge<size_t>(W*H, order, inverseOrder,
+      std::bind(ftk::Get6Neighbors2DRegular<size_t>, W, H, std::placeholders::_1));
+
+  ct.print_with_values<double>([&values](size_t i) {return values[i];});
+
   return 0;
 }
