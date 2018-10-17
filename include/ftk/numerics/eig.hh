6ef62a55909a291f09dd657f801af7525ffdd94c
Hanqi Guo <hguo@anl.gov>
2018-10-17 16:25:21 -0500

    WIP: eig3

--- include/ftk/numerics/eig.hh
+++ include/ftk/numerics/eig.hh
@@ -10,20 +10,15 @@
 namespace ftk {
 
 template <typename ValueType>
-inline void eig3(ValueType m[9], ValueType eig_[3]) 
+inline void eig3(ValueType m[9], std::complex<ValueType> eig[3], std::complex<ValueType> eigvec[3][3]) 
 {
   using std::complex;
-
-  complex<ValueType> eig[3];
 
   const ValueType b = -trace3(m),
                   c = m[4]*m[8] + m[0]*m[8] + m[0]*m[4] - m[1]*m[3] - m[5]*m[7] - m[2]*m[6],
                   d = -det3(m);
 
   cubic_solve(b, c, d, eig);
-  for (int i=0; i<3; i++) {
-    fprintf(stderr, "lambda%d=%f + i*%f\n", i, eig[i].real(), eig[i].imag());
-  }
 }
 
 }
--- tests/test_eig3.cpp
+++ tests/test_eig3.cpp
@@ -4,11 +4,12 @@
 int main(int argc, char **argv)
 {
   float m[] = {0.9102779, 0.44108077, 0.72642273, 0.39278198, 0.95680469, 0.02683596, 0.05335823, 0.86960914, 0.43971526};
-  float eig[3];
+  std::complex<float> eig[3], eigvec[3][3];
 
-  ftk::eig3(m, eig);
+  ftk::eig3(m, eig, eigvec);
 
-  fprintf(stderr, "%f, %f, %f\n", eig[0], eig[1], eig[2]);
+  for (int i=0; i<3; i++)
+    fprintf(stderr, "lambda_%d = %f + i*%f\n", i, eig[i].real(), eig[i].imag());
 
   return 0;
 }
