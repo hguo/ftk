ac7ec26bb4885866fd8b47ec369db528702e90be
Hanqi Guo <hguo@anl.gov>
2019-02-26 13:05:50 -0600

    write curves vtk

--- include/ftk/geometry/curve2vtk.hh
+++ include/ftk/geometry/curve2vtk.hh
@@ -4,29 +4,31 @@
 #include <vtkPolyData.h>
 #include <vtkPolyLine.h>
 #include <vtkCellArray.h>
+#include <vtkXMLPolyDataWriter.h>
 
 namespace ftk {
 
+template <typename T>
 vtkSmartPointer<vtkPolyData> 
-curves2vtk(const std::vector<std::vector<float>>& curves)
+curves2vtk(const std::vector<std::vector<T>>& curves)
 {
-  vtkSmartPointer<vtkPolyData> polyData = vtkPoints::New();
+  vtkSmartPointer<vtkPolyData> polyData = vtkPolyData::New();
   vtkSmartPointer<vtkPoints> points = vtkPoints::New();
   vtkSmartPointer<vtkCellArray> cells = vtkCellArray::New();
 
   for (const auto &curve : curves) {
-    for (int i = 0; i < curves.size() / 3; i ++) {
-      float p[3] = {curve[i*3], curve[i*3+1], curve[i*3+2]};
+    for (int i = 0; i < curve.size() / 3; i ++) {
+      T p[3] = {curve[i*3], curve[i*3+1], curve[i*3+2]};
       points->InsertNextPoint(p);
     }
   }
 
   size_t nv = 0;
   for (const auto &curve :curves) {
-    vtkSmartPointer<vtkPolyLine> = vtkPolyLine::New();
+    vtkSmartPointer<vtkPolyLine> polyLine = vtkPolyLine::New();
     polyLine->GetPointIds()->SetNumberOfIds(curve.size() / 3);
     for (int i = 0; i < curve.size() / 3; i ++)
-      polyLine->GetPointIds()->SetId(j, j+nv);
+      polyLine->GetPointIds()->SetId(i, i+nv);
 
     cells->InsertNextCell(polyLine);
     nv += curve.size() / 3;
@@ -36,6 +38,35 @@ curves2vtk(const std::vector<std::vector<float>>& curves)
   polyData->SetLines(cells);
 
   return polyData;
+}
+
+template <typename T>
+vtkSmartPointer<vtkPolyData>
+curves2vtk(const std::vector<std::vector<std::vector<float>>>& curve_groups)
+{
+  std::vector<std::vector<T>> curves;
+  for (const auto &g : curve_groups)
+    curves.push_back(g);
+}
+
+template <typename T>
+void write_curves_vtk(const std::vector<std::vector<std::vector<float>>>& curve_groups, const std::string& filename)
+{
+  auto poly = curves2vtk(curve_groups);
+  vtkSmartPointer<vtkXMLPolyDataWriter> writer = vtkXMLPolyDataWriter::New();
+  writer->SetFileName(filename.c_str());
+  writer->SetInputData(poly);
+  writer->Write();
+}
+
+template <typename T>
+void write_curves_vtk(const std::vector<std::vector<float>>& curves, const std::string& filename)
+{
+  auto poly = curves2vtk(curves);
+  vtkSmartPointer<vtkXMLPolyDataWriter> writer = vtkXMLPolyDataWriter::New();
+  writer->SetFileName(filename.c_str());
+  writer->SetInputData(poly);
+  writer->Write();
 }
 
 }
