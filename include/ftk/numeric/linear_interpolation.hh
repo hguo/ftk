6522389512bc80b6c329828b2dd6bff7b4569816
Hanqi Guo <hguo@anl.gov>
2019-02-27 23:28:19 -0600

    api fix

--- include/ftk/numeric/linear_interpolation.hh
+++ include/ftk/numeric/linear_interpolation.hh
@@ -47,6 +47,12 @@ inline void linear_interpolation_2simplex_matrix2x2(const T V[3][2][2], const T 
 }
 
 template <typename T>
+inline T linear_interpolation_3simplex(const T v[4], const T mu[4])
+{
+  return v[0] * mu[0] + v[1] * mu[1] + v[2] * mu[2] + v[3] * mu[3];
+}
+
+template <typename T>
 inline void linear_interpolation_3simplex_vector3(const T V[4][3], const T mu[4], T v[3])
 {
   v[0] = V[0][0] * mu[0] + V[1][0] * mu[1] + V[2][0] * mu[2] + V[3][0] * mu[3];
@@ -55,7 +61,7 @@ inline void linear_interpolation_3simplex_vector3(const T V[4][3], const T mu[4]
 }
 
 template <typename T>
-inline void linear_interpolation_3simplex_vector3(const T V[4][4], const T mu[4], T v[4])
+inline void linear_interpolation_3simplex_vector4(const T V[4][4], const T mu[4], T v[4])
 {
   v[0] = V[0][0] * mu[0] + V[1][0] * mu[1] + V[2][0] * mu[2] + V[3][0] * mu[3];
   v[1] = V[0][1] * mu[0] + V[1][1] * mu[1] + V[2][1] * mu[2] + V[3][1] * mu[3];
