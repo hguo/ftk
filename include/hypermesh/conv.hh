b289e66c3211d8ec26613f6e6f02632c0431be19
Wenbin He <hewenbin.495@gmail.com>
2019-04-17 11:51:09 -0400

    Update convolution functions

--- include/hypermesh/conv.hh
+++ include/hypermesh/conv.hh
@@ -1,7 +1,35 @@
 #ifndef _HYPERMESH_CONV_HH
 #define _HYPERMESH_CONV_HH
 
+#include <cmath>
+
+#include <string>
+
 namespace hypermesh {
+
+template <typename T>
+void MakeGaussianKernel2D(double sigma, int ksizex, int ksizey, T* kernel) {
+  // fill the kernel
+  double centerx = static_cast<double>(ksizex - 1) * .5,
+         centery = static_cast<double>(ksizey - 1) * .5;
+  double r, s = 2. * sigma * sigma;
+  double sum = 0.;
+
+  for (int j = 0; j < ksizey; ++j) {
+    for (int i = 0; i < ksizex; ++i) {
+      double x = static_cast<double>(i) - centerx,
+             y = static_cast<double>(j) - centery;
+      r = std::sqrt(x * x + y * y);
+      kernel[j * ksizex + i] = (std::exp(-(r * r) / s)) / (M_PI * s);
+      sum += kernel[j * ksizex + i];
+    }
+  }
+
+  // normalize the kernel
+  double tmp = 1. / sum;
+  for (int i = 0; i < ksizex * ksizey; ++i)
+    kernel[i] *= tmp;
+}
 
 template <typename T>
 T* Conv2D(T* data, int dimx, int dimy,
@@ -34,6 +62,23 @@ T* Conv2D(T* data, int dimx, int dimy,
       res[y * dimx_r + x] /= ksizey * ksizex;
     }
   }
+
+  return res;
+}
+
+template <typename T>
+T* Conv2DGaussian(T* data, int dimx, int dimy,
+                  double sigma, int ksizex, int ksizey,
+                  int padding=0) {
+  // make kernel
+  T* kernel = new T[ksizex * ksizey];
+  MakeGaussianKernel2D(sigma, ksizex, ksizey, kernel);
+
+  // convolution
+  T* res = Conv2D(data, dimx, dimy, kernel, ksizex, ksizey, padding);
+
+  // delete kernel
+  delete[] kernel;
 
   return res;
 }
