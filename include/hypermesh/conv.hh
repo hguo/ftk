edfae216e55075d9e39df2d79b2ff5fc81b0d45a
Wenbin He <hewenbin.495@gmail.com>
2019-04-22 14:39:11 -0500

    Update conv

--- include/hypermesh/conv.hh
+++ include/hypermesh/conv.hh
@@ -9,36 +9,6 @@ namespace hypermesh {
 
 // 2D convolutions
 template <typename T>
-ndarray<T> gaussian_kernel2D(T sigma, size_t ksizex, size_t ksizey)
-{
-  ndarray<T> kernel({ksizex, ksizey});
-
-  // fill the kernel
-  T centerx = static_cast<double>(ksizex - 1) * .5,
-    centery = static_cast<double>(ksizey - 1) * .5;
-  T r, s = T(2) * sigma * sigma;
-  T sum(0);
-
-  for (auto j = 0; j < ksizey; ++j) {
-    for (auto i = 0; i < ksizex; ++i) {
-      T x = static_cast<T>(i) - centerx,
-        y = static_cast<T>(j) - centery;
-      r = std::sqrt(x * x + y * y);
-      // kernel[j * ksizex + i] = (std::exp(-(r * r) / s)) / (M_PI * s);
-      kernel(i, j) = (std::exp(-(r * r) / s)) / (M_PI * s);
-      sum += kernel[j * ksizex + i];
-    }
-  }
-
-  // normalize the kernel
-  T tmp = T(1) / sum;
-  for (auto i = 0; i < ksizex * ksizey; ++i)
-    kernel[i] *= tmp;
-
-  return kernel;
-}
-
-template <typename T>
 ndarray<T> conv2D(const ndarray<T> &data, const ndarray<T> &kernel, size_t padding = 0)
 {
   const auto dimx = data.dim(0),
@@ -51,7 +21,6 @@ ndarray<T> conv2D(const ndarray<T> &data, const ndarray<T> &kernel, size_t paddi
 
   // resulting data
   ndarray<T> res({dimx_r, dimy_r});
-  // T* res = new T[dimx_r * dimy_r];
 
   // convolution
   // uncomment the following line to enable openmp
@@ -62,21 +31,46 @@ ndarray<T> conv2D(const ndarray<T> &data, const ndarray<T> &kernel, size_t paddi
       for (auto ky = 0; ky < ksizey; ++ky) {
         for (auto kx = 0; kx < ksizex; ++kx) {
           auto realy = y - padding + ky,
-              realx = x - padding + kx;
+               realx = x - padding + kx;
           if (realx >= 0 && realx < dimx && realy >= 0 && realy < dimy) {
             res(x, y) += data(realx, realy) * kernel(kx, ky);
-            // res[y * dimx_r + x] += data[realy * dimx + realx] *
-            //                        kernel[ky * ksizex + kx];
           }
         }
       }
 
       res(x, y) /= ksizey * ksizex;
-      // res[y * dimx_r + x] /= ksizey * ksizex;
     }
   }
 
   return res;
+}
+
+template <typename T>
+ndarray<T> gaussian_kernel2D(T sigma, size_t ksizex, size_t ksizey)
+{
+  ndarray<T> kernel({ksizex, ksizey});
+
+  // fill the kernel
+  double centerx = static_cast<double>(ksizex - 1) * .5,
+         centery = static_cast<double>(ksizey - 1) * .5;
+  double r, s = 2. * sigma * sigma;
+  double sum = 0.;
+
+  for (auto j = 0; j < ksizey; ++j) {
+    for (auto i = 0; i < ksizex; ++i) {
+      double x = static_cast<double>(i) - centerx,
+             y = static_cast<double>(j) - centery;
+      r = x * x + y * y;
+      kernel(i, j) = std::exp(-r / s);
+      sum += kernel(i, j);
+    }
+  }
+
+  // normalize the kernel
+  for (auto i = 0; i < ksizex * ksizey; ++i)
+    kernel[i] /= sum;
+
+  return kernel;
 }
 
 template <typename T>
@@ -85,111 +79,105 @@ ndarray<T> conv2D_gaussian(const ndarray<T> &data,
 {
   // make kernel
   const auto kernel = gaussian_kernel2D(sigma, ksizex, ksizey);
-  // T* kernel = new T[ksizex * ksizey];
-  // MakeGaussianKernel2D(sigma, ksizex, ksizey, kernel);
 
   // convolution
   auto res = conv2D(data, kernel, padding);
-  // T* res = Conv2D(data, dimx, dimy, kernel, ksizex, ksizey, padding);
-
-  // delete kernel
-  // delete[] kernel;
 
   return res;
 }
 
-// 3D convolutions
-template <typename T>
-T* Conv3D(T* data, int dimx, int dimy, int dimz,
-          T* kernel, int ksizex, int ksizey, int ksizez,
-          int padding=0) {
-  // dimensions for the resulting data
-  int dimx_r = dimx + padding * 2 - ksizex + 1,
-      dimy_r = dimy + padding * 2 - ksizey + 1,
-      dimz_r = dimz + padding * 2 - ksizez + 1;
+// // 3D convolutions
+// template <typename T>
+// T* Conv3D(T* data, int dimx, int dimy, int dimz,
+//           T* kernel, int ksizex, int ksizey, int ksizez,
+//           int padding=0) {
+//   // dimensions for the resulting data
+//   int dimx_r = dimx + padding * 2 - ksizex + 1,
+//       dimy_r = dimy + padding * 2 - ksizey + 1,
+//       dimz_r = dimz + padding * 2 - ksizez + 1;
 
-  // resulting data
-  T* res = new T[dimx_r * dimy_r * dimz_r];
+//   // resulting data
+//   T* res = new T[dimx_r * dimy_r * dimz_r];
 
-  // convolution
-  // uncomment the following line to enable openmp
-  // #pragma omp parallel for collapse(3)
-  for (int z = 0; z < dimz_r; ++z) {
-    for (int y = 0; y < dimy_r; ++y) {
-      for (int x = 0; x < dimx_r; ++x) {
+//   // convolution
+//   // uncomment the following line to enable openmp
+//   // #pragma omp parallel for collapse(3)
+//   for (int z = 0; z < dimz_r; ++z) {
+//     for (int y = 0; y < dimy_r; ++y) {
+//       for (int x = 0; x < dimx_r; ++x) {
 
-        for (int kz = 0; kz < ksizez; ++kz) {
-          for (int ky = 0; ky < ksizey; ++ky) {
-            for (int kx = 0; kx < ksizex; ++kx) {
-              int realz = z - padding + kz,
-                  realy = y - padding + ky,
-                  realx = x - padding + kx;
-              if (realx >= 0 && realx < dimx &&
-                  realy >= 0 && realy < dimy &&
-                  realz >= 0 && realz < dimz) {
-                res[z * dimy_r * dimx_r + y * dimx_r + x] +=
-                    data[realz * dimy * dimx + realy * dimx + realx] *
-                    kernel[kz * ksizey * ksizex + ky * ksizex + kx];
-              }
-            }
-          }
-        }
+//         for (int kz = 0; kz < ksizez; ++kz) {
+//           for (int ky = 0; ky < ksizey; ++ky) {
+//             for (int kx = 0; kx < ksizex; ++kx) {
+//               int realz = z - padding + kz,
+//                   realy = y - padding + ky,
+//                   realx = x - padding + kx;
+//               if (realx >= 0 && realx < dimx &&
+//                   realy >= 0 && realy < dimy &&
+//                   realz >= 0 && realz < dimz) {
+//                 res[z * dimy_r * dimx_r + y * dimx_r + x] +=
+//                     data[realz * dimy * dimx + realy * dimx + realx] *
+//                     kernel[kz * ksizey * ksizex + ky * ksizex + kx];
+//               }
+//             }
+//           }
+//         }
 
-        res[z * dimy_r * dimx_r + y * dimx_r + x] /=
-            ksizez * ksizey * ksizex;
-      }
-    }
-  }
+//         res[z * dimy_r * dimx_r + y * dimx_r + x] /=
+//             ksizez * ksizey * ksizex;
+//       }
+//     }
+//   }
 
-  return res;
-}
+//   return res;
+// }
 
-template <typename T>
-void MakeGaussianKernel3D(
-    double sigma, int ksizex, int ksizey, int ksizez, T* kernel) {
-  // fill the kernel
-  double centerx = static_cast<double>(ksizex - 1) * .5,
-         centery = static_cast<double>(ksizey - 1) * .5,
-         centerz = static_cast<double>(ksizez - 1) * .5;
-  double r, s = 2. * sigma * sigma;
-  double sum = 0.;
+// template <typename T>
+// void MakeGaussianKernel3D(
+//     double sigma, int ksizex, int ksizey, int ksizez, T* kernel) {
+//   // fill the kernel
+//   double centerx = static_cast<double>(ksizex - 1) * .5,
+//          centery = static_cast<double>(ksizey - 1) * .5,
+//          centerz = static_cast<double>(ksizez - 1) * .5;
+//   double r, s = 2. * sigma * sigma;
+//   double sum = 0.;
 
-  for (int j = 0; j < ksizey; ++j) {
-    for (int i = 0; i < ksizex; ++i) {
-      for (int k = 0; k < ksizez; ++k) {
-        double x = static_cast<double>(i) - centerx,
-               y = static_cast<double>(j) - centery,
-               z = static_cast<double>(k) - centerz;
-        r = x * x + y * y + z * z;
-        kernel[k * ksizex * ksizey + j * ksizex + i] = std::exp(-r/ s);
-        sum += kernel[k * ksizex * ksizey + j * ksizex + i];
-      }
-    }
-  }
+//   for (int j = 0; j < ksizey; ++j) {
+//     for (int i = 0; i < ksizex; ++i) {
+//       for (int k = 0; k < ksizez; ++k) {
+//         double x = static_cast<double>(i) - centerx,
+//                y = static_cast<double>(j) - centery,
+//                z = static_cast<double>(k) - centerz;
+//         r = x * x + y * y + z * z;
+//         kernel[k * ksizex * ksizey + j * ksizex + i] = std::exp(-r/ s);
+//         sum += kernel[k * ksizex * ksizey + j * ksizex + i];
+//       }
+//     }
+//   }
 
-  // normalize the kernel
-  double tmp = 1. / sum;
-  for (int i = 0; i < ksizex * ksizey* ksizez; ++i)
-    kernel[i] *= tmp;
-}
+//   // normalize the kernel
+//   double tmp = 1. / sum;
+//   for (int i = 0; i < ksizex * ksizey* ksizez; ++i)
+//     kernel[i] *= tmp;
+// }
 
-template <typename T>
-T* Conv3DGaussian(T* data, int dimx, int dimy, int dimz,
-                  double sigma, int ksizex, int ksizey, int ksizez,
-                  int padding=0) {
-  // make kernel
-  T* kernel = new T[ksizex * ksizey * ksizez];
-  MakeGaussianKernel3D(sigma, ksizex, ksizey, ksizez, kernel);
+// template <typename T>
+// T* Conv3DGaussian(T* data, int dimx, int dimy, int dimz,
+//                   double sigma, int ksizex, int ksizey, int ksizez,
+//                   int padding=0) {
+//   // make kernel
+//   T* kernel = new T[ksizex * ksizey * ksizez];
+//   MakeGaussianKernel3D(sigma, ksizex, ksizey, ksizez, kernel);
 
-  // convolution
-  T* res = Conv3D(data, dimx, dimy, dimz, kernel,
-                  ksizex, ksizey, ksizez, padding);
+//   // convolution
+//   T* res = Conv3D(data, dimx, dimy, dimz, kernel,
+//                   ksizex, ksizey, ksizez, padding);
 
-  // delete kernel
-  delete[] kernel;
+//   // delete kernel
+//   delete[] kernel;
 
-  return res;
-}
+//   return res;
+// }
 
 }  // namespace hypermesh
 
