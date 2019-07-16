73ae862251019386c3e8be34115a5e30927cdf46
Jiayi Xu <jiayi.xu64@gmail.com>
2019-07-16 10:01:09 -0500

    Update.

--- include/ftk/basic/distributed_union_find.hh
+++ include/ftk/basic/distributed_union_find.hh
@@ -1159,6 +1159,9 @@ void get_sets_on_p0(diy::mpi::communicator& world, diy::Master& master, diy::Con
 
     std::map<std::string, std::set<std::string>> root2set; 
 
+    #ifdef FTK_HAVE_MPI
+      std::cout<<"# of elements on proc. " << world.rank() <<" : "<< b->eles.size()<<std::endl; 
+    #endif
     for(auto& ele : b->eles) {
       if(!b->is_root(b->parent(ele))) {
         std::cout<<"Wrong! The parent is not root! "<< std::endl; 
