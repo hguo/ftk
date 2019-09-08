11a442c82bcce87c6875fc14a1973f910808da49
Jiayi Xu <jiayi.xu64@gmail.com>
2019-09-08 16:24:33 -0400

    Update.

--- examples/union_find/distributed_level_set_tracking_2d.cpp
+++ examples/union_find/distributed_level_set_tracking_2d.cpp
@@ -58,8 +58,8 @@
 #define TIME_OF_STEPS true
 #define MULTITHREAD false
 
-#define PRINT_FEATURE_DENSITY true
-#define PRINT_ELE_COUNT true
+#define PRINT_FEATURE_DENSITY false
+#define PRINT_ELE_COUNT false
 
 #define ALL_CRITICAL_POINT 0
 #define MAXIMUM_POINT      1
