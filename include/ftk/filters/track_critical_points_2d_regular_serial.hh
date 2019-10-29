3db80c86f66e23c7b4454f348c247a58b5662b6f
Hanqi Guo <hguo@anl.gov>
2019-10-29 12:51:49 -0500

    cp2dt

--- include/ftk/filters/track_critical_points_2d_regular_serial.hh
+++ include/ftk/filters/track_critical_points_2d_regular_serial.hh
@@ -55,25 +55,29 @@ protected:
   unsigned int type_filter = 0xffffffff;
   unsigned int jacobian_mode = JACOBIAN_NONE;
 
-  std::map<hypermesh::regular_simplex_mesh_element, critical_point_2dt_t> intersections;
-  std::vector<std::vector<double>> trajectories; // the output trajectories
+  typedef hypermesh::regular_simplex_mesh_element element_t;
+  
+  std::map<element_t, critical_point_2dt_t> critical_points;
+  std::vector<std::set<element_t>> connected_components;
+  std::vector<std::vector<critical_point_2dt_t>> traced_critical_points;
 
 protected:
-  bool check_simplex(const hypermesh::regular_simplex_mesh_element& s, critical_point_2dt_t& cp);
-  void trace_intersections();
-  void extract_connected_components(std::vector<std::set<hypermesh::regular_simplex_mesh_element>>& components);
+  bool check_simplex(const element_t& s, critical_point_2dt_t& cp);
+  void trace_connected_components();
 };
 
 
 ////////////////////
 void track_critical_points_2d_regular_serial::execute()
 {
+  // initializing vector fields
   if (!scalar.empty()) {
     if (V.empty()) V = hypermesh::gradient2Dt(scalar);
     if (gradV.empty()) gradV = hypermesh::jacobian2Dt(V);
     jacobian_mode = JACOBIAN_SYMMETRIC;
   }
 
+  // initializing bounds
   if (m.lb() == m.ub()) {
     if (!scalar.empty())
       m.set_lb_ub({2, 2, 0}, {static_cast<int>(V.dim(1)-3), static_cast<int>(V.dim(2)-3), static_cast<int>(V.dim(3)-1)});
@@ -81,79 +85,47 @@ void track_critical_points_2d_regular_serial::execute()
       m.set_lb_ub({0, 0, 0}, {static_cast<int>(V.dim(1)-1), static_cast<int>(V.dim(2)-1), static_cast<int>(V.dim(3)-1)});
   }
 
+  // scan 2-simplices
   fprintf(stderr, "tracking 2D critical points...\n");
   m.element_for(2, [=](hypermesh::regular_simplex_mesh_element e) {
       critical_point_2dt_t cp;
       if (check_simplex(e, cp)) {
         std::lock_guard<std::mutex> guard(mutex);
-        intersections[e] = cp;
-        // results.push_back(cp);
+        critical_points[e] = cp;
       }
     }); 
 
+  // scan 3-simplices to get connected components
   fprintf(stderr, "trace intersections...\n");
-  trace_intersections();
-}
+  union_find<element_t> uf;
+  for (const auto &kv : critical_points) 
+    uf.add(kv.first);
 
-void track_critical_points_2d_regular_serial::extract_connected_components(std::vector<std::set<hypermesh::regular_simplex_mesh_element>>& components)
-{
-  typedef hypermesh::regular_simplex_mesh_element element_t;
-
-  // Initialization
-  ftk::union_find<std::string> uf; 
-  std::map<std::string, element_t> id2ele; 
-  for (const auto &f : intersections) {
-    std::string eid = f.first.to_string(); 
-    uf.add(eid); 
-    id2ele.insert(std::make_pair(eid, f.first));
-  }
-
-  // Connected Component Labeling by using union-find. 
   m.element_for(3, [&](const hypermesh::regular_simplex_mesh_element& f) {
-    const auto elements = f.sides();
-    std::set<std::string> features; 
+    const auto sides = f.sides();
+    std::set<element_t> intersected_sides;
 
-    for (const auto& ele : elements) {
-      std::string eid = ele.to_string(); 
+    for (const auto& side : sides)
+      if (uf.has(side)) 
+        intersected_sides.insert(side);
 
-      if(uf.has(eid)) {
-        features.insert(eid); 
+    if (intersected_sides.size() > 1) {// the size of intersected_size should be 0 or 2
+      for (auto it = std::next(intersected_sides.begin(), 1); it != intersected_sides.end(); it ++) {
+        std::lock_guard<std::mutex> guard(mutex);
+        uf.unite(*intersected_sides.begin(), *it);
       }
     }
+  });
+  uf.get_sets(connected_components);
 
-    if(features.size()  > 1) {
-      for(std::set<std::string>::iterator ite_i = std::next(features.begin(), 1); ite_i != features.end(); ++ite_i) {
-        std::lock_guard<std::mutex> guard(mutex); // Use a lock for thread-save. 
-        uf.unite(*(features.begin()), *ite_i); 
-      }
-    }
-  }); 
-
-
-  // the output sets of connected elements
-  std::vector<std::set<std::string>> connected_components_str; // connected components 
-  
-  // Get disjoint sets of element IDs
-  uf.get_sets(connected_components_str);
-
-  // Convert element IDs to elements
-  for(auto& comp_str : connected_components_str) {
-    std::set<element_t>& comp = components.emplace_back(); 
-    for(auto& ele_id : comp_str) {
-      comp.insert(id2ele.find(ele_id)->second); 
-    }
-  }
+  // convert connected components to traced critical points
+  fprintf(stderr, "tracing critical points...\n");
+  trace_connected_components();
 }
 
-void track_critical_points_2d_regular_serial::trace_intersections()
+void track_critical_points_2d_regular_serial::trace_connected_components()
 {
-  typedef hypermesh::regular_simplex_mesh_element element_t; 
-
-  std::vector<std::set<element_t>> cc; // connected components 
-  extract_connected_components(cc);
-
   // Convert connected components to geometries
-
   auto neighbors = [](element_t f) {
     std::set<element_t> neighbors;
     const auto cells = f.side_of();
@@ -165,26 +137,16 @@ void track_critical_points_2d_regular_serial::trace_intersections()
     return neighbors;
   };
 
-  fprintf(stderr, "generating curves...\n");
-#if 1
-  for (int i = 0; i < cc.size(); i ++) {
+  for (const auto &component : connected_components) {
     std::vector<std::vector<double>> mycurves;
-    auto linear_graphs = ftk::connected_component_to_linear_components<element_t>(cc[i], neighbors);
+    auto linear_graphs = ftk::connected_component_to_linear_components<element_t>(component, neighbors);
     for (int j = 0; j < linear_graphs.size(); j ++) {
-      std::vector<double> mycurve, mycolors;
-      // float max_value = std::numeric_limits<float>::min();
-      for (int k = 0; k < linear_graphs[j].size(); k ++) {
-        auto p = intersections[linear_graphs[j][k]];
-        mycurve.push_back(p.x[0]); //  / (DW-1));
-        mycurve.push_back(p.x[1]); //  / (DH-1));
-        mycurve.push_back(p.x[2]); //  / (DT-1));
-        mycurve.push_back(p.scalar);
-        // max_value = std::max(max_value, p.scalar);
-      }
-      trajectories.emplace_back(mycurve);
+      std::vector<critical_point_2dt_t> traj; 
+      for (int k = 0; k < linear_graphs[j].size(); k ++)
+        traj.push_back(critical_points[linear_graphs[j][k]]);
+      traced_critical_points.emplace_back(traj);
     }
   }
-#endif
 }
 
 bool track_critical_points_2d_regular_serial::check_simplex(
@@ -216,23 +178,30 @@ bool track_critical_points_2d_regular_serial::check_simplex(
 #if FTK_HAVE_VTK
 vtkSmartPointer<vtkPolyData> track_critical_points_2d_regular_serial::get_results_vtk() const
 {
-  return curves2vtk(trajectories, 4);
-
   vtkSmartPointer<vtkPolyData> polyData = vtkPolyData::New();
   vtkSmartPointer<vtkPoints> points = vtkPoints::New();
-  vtkSmartPointer<vtkCellArray> vertices = vtkCellArray::New();
-  
-  vtkIdType pid[1];
-  for (const auto &kv : intersections) {
-    const auto &cp = kv.second;
-    double p[3] = {cp.x[0], cp.x[1], cp.x[2]};
-    pid[0] = points->InsertNextPoint(p);
-    vertices->InsertNextCell(1, pid);
+  vtkSmartPointer<vtkCellArray> cells = vtkCellArray::New();
+
+  for (const auto &curve : traced_critical_points)
+    for (int i = 0; i < curve.size(); i ++) {
+      double p[3] = {curve[i][0], curve[i][1], curve[i][2]};
+      points->InsertNextPoint(p);
+    }
+
+  size_t nv = 0;
+  for (const auto &curve : traced_critical_points) {
+    vtkSmartPointer<vtkPolyLine> polyLine = vtkPolyLine::New();
+    polyLine->GetPointIds()->SetNumberOfIds(curve.size());
+    for (int i = 0; i < curve.size(); i ++)
+      polyLine->GetPointIds()->SetId(i, i+nv);
+
+    cells->InsertNextCell(polyLine);
+    nv += curve.size();
   }
 
   polyData->SetPoints(points);
-  polyData->SetVerts(vertices);
- 
+  polyData->SetLines(cells);
+
   return polyData;
 }
 #endif
