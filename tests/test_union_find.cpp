c3ae271047b0f0de531152f5288e89af2ae9cca7
Hanqi Guo <hguo@anl.gov>
2020-07-16 23:11:54 -0500

    removing gtest dependencies

--- tests/test_union_find.cpp
+++ tests/test_union_find.cpp
@@ -1,16 +1,11 @@
-#include <gtest/gtest.h>
+#define CATCH_CONFIG_RUNNER
+#include "catch.hh"
 #include <ftk/basic/union_find.hh>
 #include <ftk/basic/simple_union_find.hh>
 #include <string>
 
-class union_find_test : public testing::Test {
-public:
-  const int nruns = 1;
-  const double epsilon = 1e-9;
-};
-
 // test (sparse) union-find
-TEST_F(union_find_test, union_find) {
+TEST_CASE("union_find") {
   ftk::union_find<std::string> UF; 
   UF.add("00"); 
   UF.add("11"); 
@@ -21,24 +16,30 @@ TEST_F(union_find_test, union_find) {
   UF.unite("11", "22");
   UF.unite("22", "33"); 
 
-  EXPECT_TRUE(UF.same_set("00", "00"));
-  EXPECT_TRUE(UF.same_set("11", "22"));
-  EXPECT_TRUE(UF.same_set("11", "33"));
+  REQUIRE(UF.same_set("00", "00"));
+  REQUIRE(UF.same_set("11", "22"));
+  REQUIRE(UF.same_set("11", "33"));
 
-  EXPECT_TRUE(!UF.same_set("00", "11"));
-  EXPECT_TRUE(!UF.same_set("11", "111"));
+  REQUIRE(!UF.same_set("00", "11"));
+  REQUIRE(!UF.same_set("11", "111"));
 }
 
 // test simple union-find
-TEST_F(union_find_test, simple_union_find) {
+TEST_CASE("simple_union_find") {
   ftk::simple_union_find<int> UF(10); 
   UF.unite(1, 2);
   UF.unite(2, 3); 
 
-  EXPECT_TRUE(UF.same_set(0, 0));
-  EXPECT_TRUE(UF.same_set(1, 2));
-  EXPECT_TRUE(UF.same_set(1, 3));
+  REQUIRE(UF.same_set(0, 0));
+  REQUIRE(UF.same_set(1, 2));
+  REQUIRE(UF.same_set(1, 3));
 
-  EXPECT_TRUE(!UF.same_set(0, 1));
-  EXPECT_TRUE(!UF.same_set(1, 5));
+  REQUIRE(!UF.same_set(0, 1));
+  REQUIRE(!UF.same_set(1, 5));
+}
+
+int main(int argc, char **argv)
+{
+  Catch::Session session;
+  return session.run(argc, argv);
 }
