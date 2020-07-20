78690b073d070c23b62f51f4afc60382c9f5c174
Hanqi Guo <hguo@anl.gov>
2020-07-19 23:35:34 -0500

    WIP: openmpi in travis

--- tests/test_critical_point_tracking.cpp
+++ tests/test_critical_point_tracking.cpp
@@ -5,6 +5,8 @@
 using nlohmann::json;
 
 TEST_CASE("critical_point_tracking_2d_woven") {
+  diy::mpi::communicator world;
+
   json ji = {
     {"type", "synthetic"},
     {"name", "woven"}
@@ -16,12 +18,14 @@ TEST_CASE("critical_point_tracking_2d_woven") {
   ftk::critical_point_tracker_wrapper consumer;
   consumer.consume(stream);
 
-  auto tracker = std::dynamic_pointer_cast<ftk::critical_point_tracker_2d_regular>( consumer.get_tracker() );
-  auto trajs = tracker->get_traced_critical_points();
-  auto points = tracker->get_discrete_critical_points();
+  if (world.rank() == 0) {
+    auto tracker = std::dynamic_pointer_cast<ftk::critical_point_tracker_2d_regular>( consumer.get_tracker() );
+    auto trajs = tracker->get_traced_critical_points();
+    auto points = tracker->get_discrete_critical_points();
 
-  REQUIRE(trajs.size() == 48);
-  // REQUIRE(points.size() == 4194); // TODO: this number seems to vary.  need to find out the reason
+    REQUIRE(trajs.size() == 48);
+    // REQUIRE(points.size() == 4194); // TODO: check out if this number varies over different platform
+  }
 }
 
 int main(int argc, char **argv)
