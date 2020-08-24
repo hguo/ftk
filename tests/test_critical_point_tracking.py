8200eefe70c4bac8e2e4b26f2bf3ee374d13a593
Hanqi Guo <hguo@anl.gov>
2020-08-23 23:33:46 -0500

    pyftk unit tests

--- tests/test_critical_point_tracking.py
+++ tests/test_critical_point_tracking.py
@@ -3,8 +3,8 @@ import pyftk
 
 class MainTest(unittest.TestCase):
     def test_critical_point_tracking_moving_extremum_2d(self):
-        data = pyftk.synth.moving_extremum(11, 13, 20, 5, 5, 0.1, 0.2)
-        result = pyftk.track_critical_points_2d_scalar(data)
+        data = pyftk.synthesizers.moving_extremum(11, 13, 20, 5, 5, 0.1, 0.2)
+        result = pyftk.trackers.track_critical_points_2d_scalar(data)
         self.assertEqual(len(result), 1)
 
 if __name__ == '__main__':
