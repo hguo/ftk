6e171c94173837f3b347b440a1509791e4aa99db
Hanqi Guo <hguo@anl.gov>
2020-10-16 19:37:39 -0500

    feature surface to vtu

--- include/ftk/filters/feature_surface.hh
+++ include/ftk/filters/feature_surface.hh
@@ -8,6 +8,7 @@
 #include <vtkUnsignedIntArray.h>
 #include <vtkPolyData.h>
 #include <vtkTriangle.h>
+#include <vtkUnstructuredGrid.h>
 #include <vtkXMLPolyDataWriter.h>
 #endif
 
@@ -19,6 +20,7 @@ struct feature_surface_t {
 
 #if FTK_HAVE_VTK
   vtkSmartPointer<vtkPolyData> to_vtp() const;
+  vtkSmartPointer<vtkUnstructuredGrid> to_vtu() const;
 #endif
 };
 
@@ -52,14 +54,48 @@ inline vtkSmartPointer<vtkPolyData> feature_surface_t::to_vtp() const
   for (int i = 0; i < conn.size(); i ++) {
     const auto &c = conn[i];
     vtkSmartPointer<vtkTriangle> tri = vtkTriangle::New();
-    tri->GetPointIds()->SetId(0, std::get<0>(c));
-    tri->GetPointIds()->SetId(1, std::get<1>(c));
-    tri->GetPointIds()->SetId(2, std::get<2>(c));
+    tri->GetPointIds()->SetId(0, c[0]);
+    tri->GetPointIds()->SetId(1, c[1]);
+    tri->GetPointIds()->SetId(2, c[2]);
     cells->InsertNextCell(tri);
   }
   poly->SetPolys(cells);
 
   return poly;
+}
+
+inline vtkSmartPointer<vtkUnstructuredGrid> feature_surface_t::to_vtu() const
+{
+  vtkSmartPointer<vtkUnstructuredGrid> grid = vtkUnstructuredGrid::New();
+  vtkSmartPointer<vtkPoints> points = vtkPoints::New();
+
+  vtkSmartPointer<vtkDataArray> array_id = vtkUnsignedIntArray::New();
+  array_id->SetName("id");
+  array_id->SetNumberOfComponents(1);
+  array_id->SetNumberOfTuples(pts.size());
+
+  vtkSmartPointer<vtkDataArray> array_time = vtkDoubleArray::New();
+  array_time->SetName("time");
+  array_time->SetNumberOfComponents(1);
+  array_time->SetNumberOfTuples(pts.size());
+
+  for (int i = 0; i < pts.size(); i ++) {
+    const auto &p = pts[i];
+    points->InsertNextPoint(p.x[0], p.x[1], p.x[2]);
+    array_id->SetTuple1(i, p.id);
+    array_time->SetTuple1(i, p.t);
+  }
+  grid->SetPoints(points);
+  grid->GetPointData()->AddArray(array_id);
+  grid->GetPointData()->AddArray(array_time);
+
+  for (int i = 0; i < conn.size(); i ++) {
+    const auto &c = conn[i];
+    vtkIdType ids[3] = {c[0], c[1], c[2]};
+    grid->InsertNextCell(VTK_TRIANGLE, 3, ids);
+  }
+
+  return grid;
 }
 #endif
 
