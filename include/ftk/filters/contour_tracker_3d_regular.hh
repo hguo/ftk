98d1ec76999b967c31be9a4dabb110039df18286
Hanqi Guo <hguo@anl.gov>
2020-11-06 17:40:23 -0600

    fixing builds

--- include/ftk/filters/contour_tracker_3d_regular.hh
+++ include/ftk/filters/contour_tracker_3d_regular.hh
@@ -313,7 +313,7 @@ inline void contour_tracker_3d_regular::simplex_values(
     scalars[i] = field_data_snapshots[iv].scalar(
         vertices[i][0] - local_array_domain.start(0), 
         vertices[i][1] - local_array_domain.start(1), 
-        vertices[i][2] - local_array_domain.start(2)}));
+        vertices[i][2] - local_array_domain.start(2));
     for (int j = 0; j < 3; j ++)
       grads[i][j] = field_data_snapshots[iv].gradient(
           j, vertices[i][0], vertices[i][1], vertices[i][2]);
