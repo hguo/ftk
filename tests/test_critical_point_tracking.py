e9c1b122b8e98151975d1ef47199e8d30c935741
Hanqi Guo <hguo@anl.gov>
2020-11-06 17:32:25 -0600

    fixing unit test

--- tests/test_critical_point_tracking.py
+++ tests/test_critical_point_tracking.py
@@ -10,7 +10,7 @@ class MainTest(unittest.TestCase):
     def test_critical_point_tracking_spiral_woven(self):
         data = pyftk.synthesizers.spiral_woven(10, 10, 20)
         result = pyftk.trackers.track_critical_points_2d_scalar(data)
-        self.assertEqual(len(result), 20)
+        self.assertEqual(len(result), 30)
 
 if __name__ == '__main__':
     unittest.main()
