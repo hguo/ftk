f8177baf9bffa7b782459c9d515d295e19a01bb0
Hanqi Guo <guohanqi@gmail.com>
2020-11-12 11:09:25 -0600

    WIP: travis paraview

--- tests/travis/install-paraview.sh
+++ tests/travis/install-paraview.sh
@@ -3,7 +3,7 @@
 curl https://www.paraview.org/paraview-downloads/download.php\?submit\=Download\&version\=v5.8\&type\=source\&os\=Sources\&downloadFile\=ParaView-v5.8.1.tar.gz -o paraview.tar.gz
 tar zxf paraview.tar.gz
 # curl -L https://github.com/Kitware/ParaView/archive/v5.8.1.tar.gz | tar zxf -
-cd ParaView-5.8.1
+cd ParaView-v5.8.1
 mkdir build && cd build
 
 cmake .. -DPARAVIEW_BUILD_EDITION=CORE -DCMAKE_INSTALL_PREFIX=$HOME/paraview-5.8.1
