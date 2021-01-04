547dc006ee16d394676e5ae93b092b61fbf1f0c9
Jiayi Xu <jiayi.xu64@gmail.com>
2021-01-04 04:14:23 -0500

    Enable load balancing.

--- examples/union_find/distributed_critical_point_tracking_2d.cpp
+++ examples/union_find/distributed_critical_point_tracking_2d.cpp
@@ -56,7 +56,7 @@
 #include <cereal/types/map.hpp>
 #include <cereal/types/vector.hpp>
 
-#define LOAD_BALANCING false
+#define LOAD_BALANCING true
 #define IS_IEXCHANGE true
 // #define DEPRECATE_DATA_WHEN_NECESSARY true
 
