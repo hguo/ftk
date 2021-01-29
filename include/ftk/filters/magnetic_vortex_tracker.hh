0996b5e00b12ea14975f27d5488e6c964bc14bc3
Hanqi Guo <hguo@anl.gov>
2021-01-29 17:12:33 -0600

    WIP: magnetic vortex tracker

--- include/ftk/filters/magnetic_vortex_tracker.hh
+++ include/ftk/filters/magnetic_vortex_tracker.hh
@@ -1,5 +1,5 @@
-#ifndef _FTK_TDGL_TRACKER_HH
-#define _FTK_TDGL_TRACKER_HH
+#ifndef _FTK_MAGNETIC_VORTEX_HH
+#define _FTK_MAGNETIC_VORTEX_HH
 
 #include <ftk/config.hh>
 #include <ftk/algorithms/cca.hh>
@@ -10,44 +10,37 @@
 #include <ftk/geometry/points2vtk.hh>
 #include <ftk/geometry/cc2curves.hh>
 #include <ftk/utils/gather.hh>
-#include <ftk/io/tdgl.hh>
+#include <ftk/io/magnetic.hh>
 #include <iomanip>
 
 namespace ftk {
-  
-struct tdgl_vortex_tracker : public virtual tracker {
-  tdgl_vortex_tracker(diy::mpi::communicator comm) : tracker(comm) {}
+
+struct magnetic_vortex_tracker : public virtual tracker {
+  magnetic_vortex_tracker(diy::mpi::communicator comm) : tracker(comm) {}
 
   int cpdims() const { return 3; }
-  
+
   virtual void update() {}; 
   void reset() {
     field_data_snapshots.clear();
   }
-  
+
 public:
   bool advance_timestep();
 
 public:
   bool pop_field_data_snapshot();
-  void push_field_data_snapshot(
-      const tdgl_metadata_t &meta,
-      const ndarray<float> &rho, 
-      const ndarray<float> &phi, 
-      const ndarray<float> &re, 
-      const ndarray<float> &im 
-  );
-  
+  void push_field_data_snapshot(const ndarray<float> &spin);
+
 protected:
   struct field_data_snapshot_t {
-    tdgl_metadata_t meta;
-    ndarray<float> rho, phi, re, im;
+    ndarray<float> spin
+      std::deque<field_data_snapshot_t> field_data_snapshots;;
   };
   std::deque<field_data_snapshot_t> field_data_snapshots;
 };
 
-/////
-inline bool tdgl_vortex_tracker::advance_timestep()
+inline bool magnetic_vortex_tracker::advance_timestep()
 {
   update_timestep();
   pop_field_data_snapshot();
@@ -56,7 +49,7 @@ inline bool tdgl_vortex_tracker::advance_timestep()
   return field_data_snapshots.size() > 0;
 }
 
-inline bool tdgl_vortex_tracker::pop_field_data_snapshot()
+inline bool magnetic_vortex_tracker::pop_field_data_snapshot()
 {
   if (field_data_snapshots.size() > 0) {
     field_data_snapshots.pop_front();
@@ -64,21 +57,10 @@ inline bool tdgl_vortex_tracker::pop_field_data_snapshot()
   } else return false;
 }
   
-inline void tdgl_vortex_tracker::push_field_data_snapshot(
-      const tdgl_metadata_t &meta,
-      const ndarray<float> &rho, 
-      const ndarray<float> &phi, 
-      const ndarray<float> &re, 
-      const ndarray<float> &im 
-  )
+inline void magnetic_vortex_tracker::push_field_data_snapshot(const ndarray<float> &spin)
 {
   field_data_snapshot_t snapshot;
-  snapshot.meta = meta;
-  snapshot.rho = rho;
-  snapshot.phi = phi;
-  snapshot.re = re;
-  snapshot.im = im;
-
+  snapshot.spin = spin;
   field_data_snapshots.emplace_back(snapshot);
 }
 
