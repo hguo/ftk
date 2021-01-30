3bef42e9085c86b09ead069069dd4d971681ed37
Hanqi Guo <hguo@anl.gov>
2021-01-30 11:09:57 -0600

    fixing build...

--- include/ftk/ndarray/synthetic.hh
+++ include/ftk/ndarray/synthetic.hh
@@ -123,7 +123,7 @@ ndarray<T> synthetic_double_gyre_unstructured(
 {
   ndarray<T> result; 
   result.reshape(coords);
-  result.set_multicomponent();
+  result.set_multicomponents();
 
   for (auto i = 0; i < coords.dim(1); i ++) {
     auto uv = double_gyre(coords(0, i), coords(1, i), time, A, omega, epsilon);
@@ -143,7 +143,7 @@ ndarray<T> synthetic_double_gyre(int DW, int DH, const T time, bool zchannel=fal
 {
   ndarray<T> Vf;
   Vf.reshape(2 + zchannel, DW, DH);
-  Vf.set_multicomponent();
+  Vf.set_multicomponents();
 
   for (int j = 0; j < DH; j ++) {
     for (int i = 0; i < DW; i ++) {
@@ -168,7 +168,7 @@ ndarray<T> synthetic_time_varying_double_gyre(
     const T A = 0.1, const T omega = M_PI * 0.2, const T epsilon = 0.25)
 {
   ndarray<T> Vft;
-  Vft.set_multicomponent();
+  Vft.set_multicomponents();
 
   for (int t = 0; t < DT; t ++) {
     auto Vf = synthetic_double_gyre(DW, DH, 
@@ -187,7 +187,7 @@ ndarray<T> synthetic_abc_flow(int DW, int DH, int DD,
 {
   ndarray<T> Vf;
   Vf.reshape(3, DW, DH, DD);
-  Vf.set_multicomponent();
+  Vf.set_multicomponents();
 
   for (int k = 0; k < DD; k ++)
     for (int j = 0; j < DH; j ++)
@@ -387,7 +387,7 @@ ndarray<T> synthetic_tornado(int xs, int ys, int zs, int time)
 {
   ndarray<T> array;
   array.reshape({3, static_cast<size_t>(xs), static_cast<size_t>(ys), static_cast<size_t>(zs)});
-  array.set_multicomponent();
+  array.set_multicomponents();
   T *tornado = &array[0];
 
   T x, y, z;
