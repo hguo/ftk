8ca11d4ff0b1000049be7fe3414e422573fcb08d
Hanqi Guo <hguo@anl.gov>
2021-11-15 13:08:38 -0600

    WIP: gpu point locator 2

---
+++ src/filters/locator2.cuh
@@ -0,0 +1,129 @@
+
+__device__ __host__
+inline bool BVHNodeD_insideQuad(const BVHNodeD &q, float x, float y)
+{
+#if __CUDA_ARCH__
+  return __fmul_rz(x-q.Ax, x-q.Bx) <= 0 && __fmul_rz(y-q.Ay, y-q.By) <= 0;
+#else
+  return (x - q.Ax) * (x - q.Bx) <= 0 && (y - q.Ay) * (y - q.By) <= 0;
+#endif
+  // return x >= q.Ax && x < q.Bx && y >= q.Ay && y < q.By;
+}
+
+__device__ __host__
+inline bool BVHNodeD_insideTriangle(const BVHNodeD &q, float x, float y, float3 &lambda, float *invdet)
+{
+#if 0
+  lambda.x = ((q.y1 - q.y2)*(x - q.x2) + (q.x2 - q.x1)*(y - q.y2)) /
+          ((q.y1 - q.y2)*(q.x0 - q.x2) + (q.x2 - q.x1)*(q.y0 - q.y2));
+  lambda.y = ((q.y2 - q.y0)*(x - q.x2) + (q.x0 - q.x2)*(y - q.y2)) /
+         ((q.y1 - q.y2)*(q.x0 - q.x2) + (q.x2 - q.x1)*(q.y0 - q.y2));
+#endif
+  const float d = invdet[q.triangleId];
+  lambda.x = ((q.y1 - q.y2)*(x - q.x2) + (q.x2 - q.x1)*(y - q.y2)) * d; 
+  lambda.y = ((q.y2 - q.y0)*(x - q.x2) + (q.x0 - q.x2)*(y - q.y2)) * d;
+  lambda.z = 1.0 - lambda.x - lambda.y;
+  // fprintf(stderr, "barycentric=%f, %f, %f\n", lambda.x, lambda.y, lambda.z);
+  return lambda.x >= 0 && lambda.y >= 0 && lambda.z >= 0;
+}
+
+__device__ __host__
+inline int BVHNodeD_locatePoint_recursive(const BVHNodeD *q, const BVHNodeD *nodes, float x, float y, float3 &lambda, float *invdet)
+{
+  if (q->triangleId >= 0) { //leaf node
+    bool succ = BVHNodeD_insideTriangle(*q, x, y, lambda, invdet);
+    if (succ) return q->triangleId;
+  } else if (BVHNodeD_insideQuad(*q, x, y)) {
+    for (int j=0; j<BVH_NUM_CHILDREN; j++) {
+      if (q->childrenIds[j] > 0) {
+        int result = BVHNodeD_locatePoint_recursive(&nodes[q->childrenIds[j]], nodes, x, y, lambda, invdet);
+        if (result >= 0) return result;
+      }
+    }
+  }
+  return -1;
+}
+
+__device__ __host__
+inline int BVHNodeD_locatePoint(BVHNodeD *nodes, float x, float y, float3 &lambda, float *invdet, int root=0)
+{
+  // float lambda.x, lambda.y, lambda.z;
+  static const int maxStackSize = 64;
+  int stack[maxStackSize];
+  int stackPos = 0;
+  stack[stackPos++] = root; // push root
+
+  while (stackPos > 0) {
+    const int i = stack[--stackPos]; // pop
+    const BVHNodeD &q = nodes[i];
+
+    // fprintf(stderr, "D_checking node %d, %f, %f, %f, %f\n", i, q.Ax, q.Ay, q.Bx, q.By);
+    // fprintf(stderr, "D_checking node %d\n", i);
+
+    if (q.triangleId >= 0) { // leaf node
+      bool succ = BVHNodeD_insideTriangle(q, x, y, lambda, invdet);
+      if (succ) return i; // q.triangleId;
+    } else if (BVHNodeD_insideQuad(q, x, y)) { // non-leaf node
+      for (int j=0; j<BVH_NUM_CHILDREN; j++) {
+        if (q.childrenIds[j] > 0)
+          stack[stackPos++] = q.childrenIds[j];
+      }
+    }
+  }
+  return -1;
+}
+
+__device__ __host__
+inline int BVHNodeD_locatePoint_coherent(BVHNodeD *bvh, int last_nid, float x, float y, float3 &lambda, float *invdet, int *neighbors)
+{
+  // check if last_nid is valid
+  if (last_nid<0) return BVHNodeD_locatePoint(bvh, x, y, lambda, invdet);
+
+  // check if in the same triangle
+  if (BVHNodeD_insideTriangle(bvh[last_nid], x, y, lambda, invdet)) return last_nid;
+
+  // check if neighbor triangles have the point
+#if 0
+  for (int i=0; i<3; i++) {
+    int triangleId = bvh[last_nid].triangleId;
+    int neighborQuadId = neighbors[triangleId*3];
+    if (neighborQuadId<0) continue;
+    else if (BVHNodeD_insideTriangle(bvh[neighborQuadId], x, y, lambda, invdet)) return neighborQuadId;
+  }
+#endif
+
+  // traverse from parents
+  // int nid = BVHNodeD_locatePoint(bvh, x, y, lambda, invdet, bvh[bvh[last_nid].parentId].parentId);
+  // int nid = BVHNodeD_locatePoint(bvh, x, y, lambda, invdet, bvh[last_nid].parentId);
+  // if (nid >= 0) return nid;
+
+  // TODO: check if in triangle neighbors of last_nid
+
+  // fallback
+  return BVHNodeD_locatePoint(bvh, x, y, lambda, invdet);
+}
+
+__device__ __host__
+inline float BVHNodeD_sample(int i0, int i1, int i2, float3 lambda, float *data) {
+  return lambda.x * data[i0] + lambda.y * data[i1] + lambda.z * data[i2];
+}
+
+__device__ __host__
+inline float2 BVHNodeD_sample2(int i0, int i1, int i2, float3 lambda, float *data) {
+  return make_float2(lambda.x * data[i0*2] + lambda.y * data[i1*2] + lambda.z * data[i2*2],
+      lambda.x * data[i0*2+1] + lambda.y * data[i1*2+1] + lambda.z * data[i2*2+1]);
+}
+
+__device__ __host__
+inline float BVHNodeD_sample(BVHNodeD* bvh, int nid, float3 lambda, float *data) {
+  const BVHNodeD &q = bvh[nid];
+  return lambda.x * data[q.i0] + lambda.y * data[q.i1] + lambda.z * data[q.i2];
+}
+
+__device__ __host__
+inline float2 BVHNodeD_sample2(BVHNodeD* bvh, int nid, float3 lambda, float *data) {
+  const BVHNodeD &q = bvh[nid];
+  return BVHNodeD_sample2(q.i0, q.i1, q.i2, lambda, data);
+  // return make_float2(lambda.x * data[q.i0*2] + lambda.y * data[q.i1*2] + lambda.z * data[q.i2*2],
+  //     lambda.x * data[q.i0*2+1] + lambda.y * data[q.i1*2+1] + lambda.z * data[q.i2*2+1]);
+}
