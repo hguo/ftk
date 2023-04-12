c5b05b9d043a93d2d062f257e6195373d64a7cb4
guo.2154 <guo.2154@cse-anc240738l.mynetworksettings.com>
2023-04-12 13:11:44 -0400

    read xgc eq from adios

--- include/ftk/io/xgc_eq.hh
+++ include/ftk/io/xgc_eq.hh
@@ -20,6 +20,11 @@ struct xgc_eq_t {
   void print() const;
 
   void eval_b(const double rz[], double b[]) const;
+  
+#if FTK_HAVE_ADIOS2
+  void read_bp(const std::string filename, diy::mpi::communicator comm = MPI_COMM_WORLD);
+  void read_bp(adios2::IO &io, adios2::Engine& reader);
+#endif
 
 #if FTK_HAVE_HDF5
   void to_h5(const std::string filename) const;
@@ -30,7 +35,7 @@ struct xgc_eq_t {
   void to_vtr_file(const std::string filename) const;
 #endif
 
-  size_t mr, mz, mpsi;
+  int mr, mz, mpsi;
   double min_r, max_r, min_z, max_z; 
   double axis_r, axis_z, axis_b;
   double x_psi, x_r, x_z;
@@ -118,7 +123,7 @@ inline void xgc_eq_t::parse(const std::string& filename)
 
 inline void xgc_eq_t::print() const 
 {
-  fprintf(stderr, "mr=%zu, mz=%zu, mpsi=%zu\n", mr, mz, mpsi);
+  fprintf(stderr, "mr=%d, mz=%d, mpsi=%d\n", mr, mz, mpsi);
   fprintf(stderr, "min_r=%f, max_r=%f, min_z=%f, max_z=%f\n", 
       min_r, max_r, min_z, max_z);
   fprintf(stderr, "axis_r=%f, axis_z=%f, axis_b=%f\n", 
@@ -126,6 +131,51 @@ inline void xgc_eq_t::print() const
   fprintf(stderr, "x_psi=%f, x_r=%f, x_z=%f\n", 
       x_psi, x_r, x_z);
 }
+
+#if FTK_HAVE_ADIOS2
+inline void xgc_eq_t::read_bp(const std::string f, diy::mpi::communicator comm)
+{
+#if ADIOS2_USE_MPI
+  adios2::ADIOS adios(comm);
+#else 
+  adios2::ADIOS adios;
+#endif
+
+  adios2::IO io = adios.DeclareIO("BPReader");
+  adios2::Engine reader = io.Open(f, adios2::Mode::Read);
+
+  this->read_bp(io, reader);
+
+  reader.Close();
+}
+
+inline void xgc_eq_t::read_bp(adios2::IO &io, adios2::Engine& reader)
+{
+  this->I.read_bp(io, reader, "eq_I");
+  this->psi_rz.read_bp(io, reader, "eq_psi_rz");
+  this->psigrid.read_bp(io, reader, "eq_psi_grid");
+
+  auto read_int = [&reader](const std::string v, int& p) { reader.Get<int>(v, p); };
+  auto read_double = [&reader](const std::string v, double& p) { reader.Get<double>(v, p); };
+
+  read_int("eq_mpsi", this->mpsi);
+  read_int("eq_mr", this->mr);
+  read_int("eq_mz", this->mz);
+
+  read_double("eq_axis_r", this->axis_r);
+  read_double("eq_axis_z", this->axis_z);
+  read_double("eq_axis_b", this->axis_b);
+  
+  read_double("eq_min_r", this->min_r);
+  read_double("eq_max_r", this->max_r);
+  read_double("eq_min_z", this->min_z);
+  read_double("eq_max_z", this->max_z);
+  
+  read_double("eq_x_psi", this->x_psi);
+  read_double("eq_x_r", this->x_r);
+  read_double("eq_x_z", this->x_z);
+}
+#endif
 
 #if FTK_HAVE_HDF5
 inline void xgc_eq_t::to_h5(const std::string f) const
