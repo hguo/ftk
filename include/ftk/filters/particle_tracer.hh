8e3a80a658cd68b82bc39052af1b83cbdd515dc8
guo.2154 <guo.2154@cse-anc240738l.mynetworksettings.com>
2023-04-25 21:32:39 -0400

    tracer

--- include/ftk/filters/particle_tracer.hh
+++ include/ftk/filters/particle_tracer.hh
@@ -8,10 +8,10 @@ namespace ftk {
 
 struct particle_tracer : public virtual tracker
 {
-  particle_tracer(diy::mpi::communicator comm) : tracker(comm) {}
+  particle_tracer(diy::mpi::communicator comm, int nd /*spatial dim*/) : tracker(comm), nd_(nd) {}
   virtual ~particle_tracer() {}
 
-  virtual void set_particles(const ndarray<double> &p) {} //  particles = p; }
+  virtual void initialize_particles(size_t n, const double *p, size_t stride=3, bool has_time=false);
   virtual void initialize_particles_at_grid_points() {};
 
   void update() {}
@@ -23,11 +23,19 @@ struct particle_tracer : public virtual tracker
 protected:
   // virtual bool eval_v(const double *x, double *v) = 0;
 
+  int nd() const { return nd_; }
+  double delta() const { return current_delta_t / nsteps_per_interval; }
+
 protected:
   std::vector<feature_point_lite_t> particles;
   feature_curve_set_t trajectories;
 
   double current_t = 0.0, current_delta_t = 1.0;
+
+  int nsteps_per_interval = 128;
+  int nsteps_per_checkpoint = 16;
+
+  const int nd_;
 };
 
 //// 
@@ -49,6 +57,25 @@ inline void particle_tracer::write_trajectories(const std::string& filename)
 #endif
 }
 
+inline void particle_tracer::initialize_particles(size_t n, const double *buf, size_t stride, bool has_time)
+{
+  assert(stride >= nd());
+
+  for (auto i = 0; i < n; i ++) {
+    feature_curve_t curve;
+    feature_point_t p;
+   
+    for (int k = 0; k < nd(); k ++)
+      p.x[k] = buf[i*stride+k];
+
+    if (has_time)
+      p.t = buf[i*stride+nd()];
+
+    curve.push_back(p);
+    trajectories.add(curve);
+  }
 }
+
+} // namespace ftk
 
 #endif
