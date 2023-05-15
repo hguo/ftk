1d1de24240e26d292ea021352f8c83aa044840e8
guo.2154 <guo.2154@cse-anc240738l.mynetworksettings.com>
2023-05-15 11:31:35 -0400

    moving mpas vertlical layer search

--- include/ftk/numeric/mpas.hh
+++ include/ftk/numeric/mpas.hh
@@ -6,8 +6,9 @@
 
 namespace ftk {
 
-template <typename F>
-bool point_in_mpas_cell(const int nverts, const F Xv[][3], const F x[3])
+template <typename F=double>
+__device__ __host__ 
+inline bool point_in_mpas_cell(const int nverts, const F Xv[][3], const F x[3])
 {
   F n[3];
   for (int i = 0; i < nverts; i ++) {
@@ -19,6 +20,56 @@ bool point_in_mpas_cell(const int nverts, const F Xv[][3], const F x[3])
   }
 
   return true;
+}
+
+template <typename F=double>
+__device__ __host__
+inline int locate_mpas_layer_brute_force(const int nlayers, const F zTop[], const F z)
+{
+  int i0 = 0, i1 = nlayers;
+  // I ib = locate_layer_bisection(nlayers, zTop, z);
+
+  // fprintf(stderr, "z=%f, ib=%d\n", z, ib);
+  // for (int i = 0; i < nlayers; i ++)
+  //   fprintf(stderr, "i=%d, ztop=%f\n", i, zTop[i]);
+
+  for (int i = 0; i < nlayers; i ++)
+    if (z <= zTop[i] && z > zTop[i+1])
+      return i;
+
+  return -1;
+}
+
+template <typename F=double>
+__device__ __host__
+inline int locate_mpas_layer_bisection(const int nlayers, const F zTop[], const F z)
+{
+  int i0 = 0, i1 = nlayers-1;
+  // fprintf(stderr, "z=%f, zTop_i0=%f, zTop_i1=%f\n", z, zTop[i0], zTop[i1]);
+
+  if (z > zTop[i0] || z <= zTop[i1])
+    return -1;
+
+#if 0
+  fprintf(stderr, "--z=%f\n", z);
+  for (int i = 0; i < nlayers; i ++)
+    fprintf(stderr, "--i=%d, ztop=%f\n", i, zTop[i]);
+  // exit(1);
+#endif
+
+  while (1) {
+    if (z <= zTop[i0] && z > zTop[i0+1])
+      return i0;
+    
+    int ic = (i0 + i1) / 2;
+    // fprintf(stderr, "ic=%d\n", ic);
+    if (z - zTop[ic] <= 0)
+      i0 = ic;
+    else 
+      i1 = ic;
+  }
+
+  return -1;
 }
 
 }
